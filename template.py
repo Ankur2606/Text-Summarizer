c9d12d301dc9b9417a015762430c6cf83b4ceac2
Bhavya Pratap Singh Tomar <bhavyapratapsinghtomar@gmail.com>
2023-10-14 15:01:02 +0530

    Requirement/setup installation with notebk exp creation

--- template.py
+++ template.py
@@ -39,7 +39,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    
+ 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
