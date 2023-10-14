3b979bd2e0f467c56f672b2f0bd57ba71dcd90ac
Bhavya Pratap Singh Tomar <bhavyapratapsinghtomar@gmail.com>
2023-10-15 03:47:48 +0530

    added data evaluation and model trainer pipeline

--- src/textSummarizer/conponents/model_trainer.py
+++ src/textSummarizer/conponents/model_trainer.py
@@ -32,7 +32,7 @@ class ModelTrainer:
 
 
         trainer_args = TrainingArguments(
-            output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
+            output_dir=self.config.root_dir, num_train_epochs=10, warmup_steps=500,
             per_device_train_batch_size=1, per_device_eval_batch_size=1,
             weight_decay=0.01, logging_steps=10,
             evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
