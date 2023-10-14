49f610ab85fdcd0b9d75a98febb981af918a9d06
Bhavya Pratap Singh Tomar <bhavyapratapsinghtomar@gmail.com>
2023-10-15 01:25:29 +0530

    added data transformation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
@@ -31,15 +31,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Data Transformation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_transformation = DataTransformationTrainingPipeline()
-#    data_transformation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -50,18 +50,18 @@ class ConfigurationManager:
         return data_validation_config
     
 
-    # def get_data_transformation_config(self) -> DataTransformationConfig:
-    #     config = self.config.data_transformation
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
 
-    #     create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
-    #     data_transformation_config = DataTransformationConfig(
-    #         root_dir=config.root_dir,
-    #         data_path=config.data_path,
-    #         tokenizer_name = config.tokenizer_name
-    #     )
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
 
-    #     return data_transformation_config
+        return data_transformation_config
     
 
 
